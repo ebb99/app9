09754ef715470c48649055662eae0058726e3dba
Eduard Ballweg <eduard.ballweg@gmail.com>
2026-01-09 10:29:22 +0100

    bbb

--- app9 railway.session.sql
+++ app9 railway.session.sql
@@ -1,2 +1,10 @@
-ALTER TABLE users
-ADD CONSTRAINT users_name_unique UNIQUE (name);
+CREATE TABLE spiele (
+   id SERIAL PRIMARY KEY,
+anstoss TIMESTAMP WITH TIME ZONE NOT NULL,
+   heimverein TEXT NOT NULL,
+   gastverein TEXT NOT NULL,
+   heimtore INTEGER ,
+   gasttore INTEGER ,
+   statuswort TEXT NOT NULL
+);
+
